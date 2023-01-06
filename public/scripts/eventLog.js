ccda51e11b3e710d9d2a74b690d7aa4921ccd17e
348670050 <haloday873@gmail.com>
2023-01-06 15:41:47 -0500

    event log

--- public/scripts/eventLog.js
+++ public/scripts/eventLog.js
@@ -12,6 +12,17 @@ var EventLog = {
         this.addEvent("I DIED fgbhnjkjhgfv vbtcfyvgushbk jladmasjbadakjdjj asdjasbd jksabkjdsabjd bsakdbsadbkjsbnm", null)
         this.addEvent("I DIED fgbhnjkjhgfv vbtcfyvgushbk jladmasjbadakjdjj asdjasbd jksabkjdsabjd bsakdbsadbkjsbnm", null)
         this.addEvent("I DIED fgbhnjkjhgfv vbtcfyvgushbk jladmasjbadakjdjj asdjasbd jksabkjdsabjd bsakdbsadbkjsbnm", null)
+
+		//DEBUG
+		// let input = $('<input>').attr({
+		// 	id: 'inp'
+		// })
+		// input.prependTo('div#main')
+		// // <button id="btbt">CLICK</button>
+		// $('#btbt').click(function() {
+		// 	EventLog.addEvent($('#inp').val(), null)
+			
+		// })
     },
     
 	addEvent: function(text, module) {
@@ -31,7 +42,20 @@ var EventLog = {
 		console.log(t)
 		let text = $('<div>').addClass('eventTxt').css('opacity', '0').text(t).prependTo('div#eventlog');
 		text.animate({opacity: 1}, 1000, 'linear', function() {
-			// EventLog.clearHidden();
+			EventLog.clearHidden();
 		});
-    }
+    },
+	
+	clearHidden: function() {
+		let bottom = $('#eventlog').position().top + $('#eventlog').outerHeight(true);;
+		console.log(bottom)
+		$('.eventTxt').each(function() {
+		
+			if($(this).position().top > bottom){
+				$(this).remove();
+			}
+		
+		});
+		
+	},
 }

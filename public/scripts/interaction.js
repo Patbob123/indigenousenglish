7d54ca3754dae09f4d00718562407a361abf082c
348670050 <haloday873@gmail.com>
2023-01-07 02:43:18 -0500

    what

--- public/scripts/interaction.js
+++ public/scripts/interaction.js
@@ -7,21 +7,28 @@ var Interaction = {
         this.addInter('a')
 
         $('#menu').append(this.inter)
+        sm.set('features.Earth.walk', true)
     },
 
     addInter: async function(goto) { 
-        let a = $('<a>')
-        let interBtn = await $('<div>').addClass('interBtn')
-            .attr({
-                id: 'a',
-                index: 0
-            })
-            .css('opacity', 0)
-            .click(this.clcicked)
-            interBtn.append(a)
-            interBtn.animate({opacity: 1}, 1000, 'linear');
-          
-        this.inter.append(interBtn)
+
+        let curPlanet = sm.get('planets.curPlanet')==-1?0:sm.get('planets.curPlanet')
+        let planetBtns = eval(Navigation.planetList[curPlanet]["Name"]).planetBtns;
+
+        for(let i in planetBtns){
+            if(sm.get('features.'+Navigation.planetList[curPlanet]["Name"]+"."+i) == true){
+                let a = $('<a>')
+                let interBtn = planetBtns[i]
+                interBtn.append(a)
+                interBtn.animate({opacity: 1}, 1000, 'linear');
+
+            this.inter.append(interBtn)
+            }else{
+                sm.set('features.'+Navigation.planetList[curPlanet]["Name"]+"."+i, false)
+            }
+        }
+
+
 
     },
     clcicked: function(){
